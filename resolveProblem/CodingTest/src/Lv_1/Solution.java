0827e2f63644526ba1f4962e6f4f6a83417eb500
sunghomong <kidcojsh@gmail.com>
2023-10-11 10:52:54 +0900

    Add CodingTest

---
+++ resolveProblem/CodingTest/src/Lv_1/Solution.java
@@ -0,0 +1,12 @@
+package Lv_1;
+
+public class Solution {
+	public int solution(int n) {
+		int answer = 0;
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return answer;
+	}
+}
