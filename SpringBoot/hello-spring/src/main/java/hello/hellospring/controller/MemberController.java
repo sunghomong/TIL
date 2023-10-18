c8ef2ec158e2d1bfa68465703ade271a48c114d9
sunghomong <kidcojsh@gmail.com>
2023-10-18 17:12:47 +0900

    Add Spring Boot TIL

--- SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,4 +20,28 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+
+    @GetMapping("/members/new") // 조회 할때 주로 사용
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new") // 값을 전달 할때 주로 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
--- SpringBoot/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,11 +2,13 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // @Service
+@Transactional // jpa를 위한 어노테이션
 public class MemberService {
 
     private final MemberRepository memberRepository;
